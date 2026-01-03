a71a8fb7dc72149702271405e55c59f160b0d6ad
richard <m.richardp93@gmail.com>
2026-01-03 11:45:34 -0500

    added looping capability to the agent so that it can continue to a specified max number of iterations till it fixes an issue or exits out gracefully

--- main.py
+++ main.py
@@ -20,7 +20,14 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     client = genai.Client(api_key=api_key)
-    generate_model_content(client, messages, args.verbose)
+
+
+    for _ in range(20): 
+        if not generate_model_content(client, messages, args.verbose): 
+            break
+    else:
+        print("LLM hasn't come to final conclusion within specified maximum iterations")
+        exit(1)
     
 
 def generate_model_content(client, messages, verbose):
@@ -31,6 +38,10 @@ def generate_model_content(client, messages, verbose):
                 tools=[available_functions],
                 system_instruction=system_prompt),
         )
+    if len(response.candidates) > 0: 
+        for candidate in response.candidates: 
+            messages.append(candidate.content)
+    
     if response.usage_metadata == None: 
         raise RuntimeError("usage metadata returned as none, API request most likely failed.")
     
@@ -43,7 +54,7 @@ def generate_model_content(client, messages, verbose):
 
     if response.function_calls == None: 
         print(f"{response.text}")
-        return 
+        return False
     list_func_results = []
     for obj in response.function_calls:
         function_call_result = call_function(obj, verbose)
@@ -57,6 +68,12 @@ def generate_model_content(client, messages, verbose):
             list_func_results.append(function_call_result.parts[0])
             if verbose: 
                 print(f" -> {function_call_result.parts[0].function_response.response}")
+    
+    
+    if len(list_func_results) > 0: 
+        messages.append(types.Content(role="user", parts=list_func_results))
+    
+    return True
 
 if __name__ == "__main__":
     main()
